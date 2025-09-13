1105b573eee9122a26ad8330ac634f9bccd899ed
Gerard Donnelly <gerrypdonnelly@gmail.com>
2025-09-13 09:23:40 +0100

    made changes to WDT

--- Stm32F103_Blue_Pill/MAKE/W_WDT_1sec/Src/WDT.c
+++ Stm32F103_Blue_Pill/MAKE/W_WDT_1sec/Src/WDT.c
@@ -3,25 +3,25 @@
 
 void IWDG_init(void)
 {
-	//Enable LSI oscillator(needed for IWDG)
+	// Enable LSI oscillator(needed for IWDG)
 	RCC->CSR |= RCC_CSR_LSION;
-	while(!(RCC->CSR & RCC_CSR_LSIRDY)); //wait for LSI to be ready
+	while (!(RCC->CSR & RCC_CSR_LSIRDY))
+		; // wait for LSI to be ready
 
-	//Enable access to IWDG registers
+	// Enable access to IWDG registers
 	IWDG->KR = 0x5555;
 
-	//Set the prescaler (divide LSI clock of ~40kHz
-	IWDG->PR = 0x06; //Prescaler 256 (40kHz /256 = ~156Hz)
+	// Set the prescaler (divide LSI clock of ~40kHz
+	IWDG->PR = 0x06; // Prescaler 256 (40kHz /256 = ~156Hz)
 
-	//Set the reload value (defines the timeout period)
-	IWDG->RLR = 156; //Timeout = (RLR+1)/156Hz =~1s
+	// Set the reload value (defines the timeout period)
+	IWDG->RLR = 156; // Timeout = (RLR+1)/156Hz =~1s
 
-	//Start the watchdog
+	// Start the watchdog
 	IWDG->KR = 0xCCCC;
 }
 
 void IWDG_Refresh(void)
 {
-	IWDG->KR = 0xAAAA; //Reload the watchdog counter
+	IWDG->KR = 0xAAAA; // Reload the watchdog counter
 }
-
--- Stm32F103_Blue_Pill/MAKE/W_WDT_1sec/Src/main.c
+++ Stm32F103_Blue_Pill/MAKE/W_WDT_1sec/Src/main.c
@@ -3,33 +3,30 @@
 #include "usart.h"
 #include "WDT.h"
 
-
 int main(void)
 {
-    IWDG_init();  //Initialize the WDT
+    IWDG_init(); // Initialize the WDT
 
-    //Configure LED
-    RCC->APB2ENR |=(1U<<3);//Enable clock to PB
-    //Set Mode of LED pin to output max speed 50Mhz
-    GPIOB->CRL |=(1U<<0);
-    GPIOB->CRL |=(1U<<1);
-    //Set CNF of LED pin to Output push pull
-    GPIOB->CRL &=~(1U<<2);
-    GPIOB->CRL &=~(1U<<3);
-
+    // Configure LED
+    RCC->APB2ENR |= (1U << 3); // Enable clock to PB
+    // Set Mode of LED pin to output max speed 50Mhz
+    GPIOB->CRL |= (1U << 0);
+    GPIOB->CRL |= (1U << 1);
+    // Set CNF of LED pin to Output push pull
+    GPIOB->CRL &= ~(1U << 2);
+    GPIOB->CRL &= ~(1U << 3);
 
     while (1)
     {
-        //Turn on and off the LED with a delay
-        for(volatile uint16_t i=0;i<50000;i++);//Simulated workload
-    	GPIOB->ODR &=~(1U<<0); //Turn on LED
-    	for(volatile uint16_t i=0;i<50000;i++);//Simulated workload
-    	GPIOB->ODR |=(1U<<0); //Turn on LED
-    	for(volatile uint16_t i=0;i<1000000;i++);//Simulated workload
-    	IWDG_Refresh(); //Reset the watchdog to prevent system reset
+        // Turn on and off the LED with a delay
+        for (volatile uint16_t i = 0; i < 50000; i++)
+            ;                     // Simulated workload
+        GPIOB->ODR &= ~(1U << 0); // Turn on LED
+        for (volatile uint16_t i = 0; i < 50000; i++)
+            ;                    // Simulated workload
+        GPIOB->ODR |= (1U << 0); // Turn on LED
+        for (volatile uint16_t i = 0; i < 1000000; i++)
+            ;           // Simulated workload
+        IWDG_Refresh(); // Reset the watchdog to prevent system reset
     }
 }
-
-
-
-
