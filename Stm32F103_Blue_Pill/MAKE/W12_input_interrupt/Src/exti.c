3f36016a834b38a85d75eace9fb02d60db59bddb
Ger <gerrypdonnelly@gmail.com>
2025-11-14 23:32:26 +0000

    made changes to W12 interrupt

--- Stm32F103_Blue_Pill/MAKE/W12_input_interrupt/Src/exti.c
+++ Stm32F103_Blue_Pill/MAKE/W12_input_interrupt/Src/exti.c
@@ -16,23 +16,27 @@ LED PB2 APB2
 void PA0_exti_init(void)
 {
 	//Disable global interrupts
+	//Enable the AFIO clock
 	//Enable clock access for GPIO A
-	////////////enable clock access for syscfg
-	//select port a for exti
-	//unmask exti
-	//select rising or falling edge trigger
-	//enable exti in NVIC
-	//enable global interrupts
+	//Ensure pin as configured as input
+	//Ensure pin as configured as input
+	//Select port a for exti
+	//Unmask exti
+	//Select rising edge trigger
+	//Enable exti in NVIC	
+	//Enable global interrupts
 
 
-	__disable_irq;					//Disable global interrupts
-	RCC->APB2ENR |=(1U<<2);			//Enable clock access for GPIO A
+	__disable_irq();			//Disable global interrupts
+	RCC->APB2ENR |=(1U<<0);		//Enable the AFIO clock
+	RCC->APB2ENR |=(1U<<2);		//Enable clock access for GPIO A
+	GPIOA->CRL &=~(1U<<0); 		//Ensure pin as configured as input
+	GPIOA->CRL &=~(1U<<1); 		//Ensure pin as configured as input
+	AFIO_EX &=~(1U<<0);//Select port a for exti  Fuck knows !!??!
+	EXTI->IMR |=(1U<<0);		//Unmask exti
+	EXTI->RTSR |=(1U<<0);		//Select rising edge trigger
+	NVIC_EnableIRQ(EXTI0_IRQn);	//Enable exti in NVIC	
 
-		//enable clock access for syscfg
-		//select port a for exti
-		//unmask exti
-		//select rising or falling edge trigger
-		//enable exti in NVIC
-	__disable_irq;//enable global interrupts
+	__enable_irq();//Enable global interrupts
 }
 
