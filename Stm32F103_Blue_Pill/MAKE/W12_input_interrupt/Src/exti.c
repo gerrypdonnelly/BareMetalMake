7c4c8463e70764ec7df90d1415aa7ffc7429ec76
Gerard Donnelly <gerrypdonnelly@gmail.com>
2025-11-17 11:36:42 +0000

    got W12_interrupt to compile and flash

--- Stm32F103_Blue_Pill/MAKE/W12_input_interrupt/Src/exti.c
+++ Stm32F103_Blue_Pill/MAKE/W12_input_interrupt/Src/exti.c
@@ -12,32 +12,28 @@ LED PB2 APB2
 
 #include "exti.h"
 
-
 void PA0_exti_init(void)
 {
-	//Disable global interrupts
-	//Enable the AFIO clock
-	//Enable clock access for GPIO A
-	//Ensure pin as configured as input
-	//Ensure pin as configured as input
-	//Select port a for exti
-	//Unmask exti
-	//Select rising edge trigger
-	//Enable exti in NVIC	
-	//Enable global interrupts
+	// Disable global interrupts
+	// Enable the AFIO clock
+	// Enable clock access for GPIO A
+	// Ensure pin as configured as input
+	// Ensure pin as configured as input
+	// Select port a for exti
+	// Unmask exti
+	// Select rising edge trigger
+	// Enable exti in NVIC
+	// Enable global interrupts
 
+	__disable_irq();						// Disable global interrupts
+	RCC->APB2ENR |= (1U << 0);				// Enable the AFIO clock
+	RCC->APB2ENR |= (1U << 2);				// Enable clock access for GPIO A
+	GPIOA->CRL &= ~(1U << 0);				// Ensure pin as configured as input
+	GPIOA->CRL &= ~(1U << 1);				// Ensure pin as configured as input
+	AFIO->EXTICR[0] &= ~AFIO_EXTICR1_EXTI0; // Select porta for exti by ensuring all 0
+	EXTI->IMR |= (1U << 0);					// Unmask interrupt exti
+	EXTI->RTSR |= (1U << 0);				// Select rising edge trigger
+	NVIC_EnableIRQ(EXTI0_IRQn);				// Enable exti in NVIC
 
-	__disable_irq();			//Disable global interrupts
-	RCC->APB2ENR |=(1U<<0);		//Enable the AFIO clock
-	RCC->APB2ENR |=(1U<<2);		//Enable clock access for GPIO A
-	GPIOA->CRL &=~(1U<<0); 		//Ensure pin as configured as input
-	GPIOA->CRL &=~(1U<<1); 		//Ensure pin as configured as input
-	AFIO->EXTICR[0] &=~AFIO_EXTICR1_EXTI0; //Select porta for exti by ensuring all 0
-	EXTI->IMR |=(1U<<0);		//Unmask interrupt exti
-	
-	EXTI->RTSR |=(1U<<0);		//Select rising edge trigger
-	NVIC_EnableIRQ(EXTI0_IRQn);	//Enable exti in NVIC	
-
-	__enable_irq();//Enable global interrupts
+	__enable_irq(); // Enable global interrupts
 }
-
