9e531de107f2577dc44aad3b5265e900ebaf832d
Ger <gerrypdonnelly@gmail.com>
2025-11-22 23:47:22 +0000

    Fixed W_WDT

--- Stm32F103_Blue_Pill/MAKE/W_WDT_1sec/Src/kernel.c
+++ Stm32F103_Blue_Pill/MAKE/W_WDT_1sec/Src/kernel.c
@@ -7,28 +7,28 @@ int main(void)
 {
     IWDG_init(); // Initialize the WDT
 
-    // Configure LED
-    RCC->APB2ENR |= (1U << 3); // Enable clock to PB
-    // Set Mode of LED pin to output max speed 50Mhz
-    GPIOB->CRL |= (1U << 0);
-    GPIOB->CRL |= (1U << 1);
+    // Configure LEDPB2
+    RCC->APB2ENR |= (1U << 3); // Enable clock to port B
+    // Set Mode of LED pin 2 to output max speed 50Mhz
+    GPIOB->CRL |= (1U << 8);
+    GPIOB->CRL |= (1U << 9);
     // Set CNF of LED pin to Output push pull
-    GPIOB->CRL &= ~(1U << 2);
-    GPIOB->CRL &= ~(1U << 3);
+    GPIOB->CRL &= ~(1U << 10);
+    GPIOB->CRL &= ~(1U << 11);
 
     while (1)
     {
         // Turn on and off the LED with a delay
-        for (volatile uint16_t i = 0; i < 5000; i++)
+        for (volatile uint16_t i = 0; i < 50000; i++)
             ;                     // Simulated workload
-        GPIOB->ODR &= ~(1U << 0); // Turn off LED
+        GPIOB->ODR &= ~(1U << 2); // Turn off LED
 
-        for (volatile uint16_t i = 0; i < 5000; i++)
+        for (volatile uint16_t i = 0; i < 50000; i++)
             ;                    // Simulated workload
-        GPIOB->ODR |= (1U << 0); // Turn on LED
+        GPIOB->ODR |= (1U << 2); // Turn on LED
 
-        for (volatile uint16_t i = 0; i < 100; i++)
-            ;           // Simulated workload
+        for (volatile uint16_t i = 0; i < 50000; i++) 
+            ;           // Simulated workload increase this to test WDT reset 1sec value 100000
         IWDG_Refresh(); // Reset the watchdog to prevent system reset
     }
 }
