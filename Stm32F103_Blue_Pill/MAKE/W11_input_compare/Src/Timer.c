48e013dc456f6081ced5911a92604a7b3541eee8
Gerard Donnelly <gerrypdonnelly@gmail.com>
2025-12-08 19:13:00 +0000

    made changes to W11 input compare

--- Stm32F103_Blue_Pill/MAKE/W11_input_compare/Src/Timer.c
+++ Stm32F103_Blue_Pill/MAKE/W11_input_compare/Src/Timer.c
@@ -24,93 +24,82 @@
 
 #include "stm32f103xb.h"
 
-#define TIM2EN (1U<<0)
-#define TIM3EN (1U<<1)
-#define CR1_CEN (1U<<0)
-#define OC_TOGGLE (1U<<4 |1U<<5)
-#define CCER_CC3E (1U<<8)
-#define TIM4EN (1U<<2)
-#define CCER_CC4E (1U<<12)
+#define TIM2EN (1U << 0)
+#define TIM3EN (1U << 1)
+#define CR1_CEN (1U << 0)
+#define OC_TOGGLE (1U << 4 | 1U << 5)
+#define CCER_CC3E (1U << 8)
+#define TIM4EN (1U << 2)
+#define CCER_CC4E (1U << 12)
 
-
-//Uses Timer2 to time out every one second 1Hz
+// Uses Timer2 to time out every one second 1Hz
 void Tim2_1hz_init(void)
 {
-	RCC->APB1ENR |= TIM2EN;		//Enable clock access to timer 2
-	TIM2->PSC =800 - 1;  		//Set pre-scaler value in pre-scaler register 8 000 000 /800 = 10000
-	TIM2->ARR = 10000 - 1; 		//set auto reload value 10000/10000 = 1Hz
-	TIM2->CNT = 0; 				//clear the counter value, write 0 into timer count register
-	TIM2->CR1 |= CR1_CEN;		//enable the timer
+	RCC->APB1ENR |= TIM2EN; // Enable clock access to timer 2
+	TIM2->PSC = 800 - 1;	// Set pre-scaler value in pre-scaler register 8 000 000 /800 = 10000
+	TIM2->ARR = 10000 - 1;	// set auto reload value 10000/10000 = 1Hz
+	TIM2->CNT = 0;			// clear the counter value, write 0 into timer count register
+	TIM2->CR1 |= CR1_CEN;	// enable the timer
 }
-
-
-
-
 
 void Tim3_PB0_output_compare(void)
 {
-	//PB0 is connected to TIM3_CH3 by default or can be re-mapped to TIM1_CH2
+	// PB0 is connected to TIM3_CH3 by default or can be re-mapped to TIM1_CH2
 
-	//Enable clock access to GPIO B
-	RCC->APB2ENR |=(1U<<3);
-	//SET PB0 mode to "alternate" function mode
-	//Set Mode of LED pin to output max speed 2Mhz
-	GPIOB->CRL |=(1U<<1);
-	GPIOB->CRL &=~(1U<<0);
-		//Set CNF of LED pin to alternate function output push pull
-	GPIOB->CRL |=(1U<<3);
-	GPIOB->CRL &=~(1U<<2);
+	// Enable clock access to GPIO B
+	RCC->APB2ENR |= (1U << 3);
+	// SET PB0 mode to "alternate" function mode
+	// Set Mode of LED pin to output max speed 2Mhz
+	GPIOB->CRL |= (1U << 1);
+	GPIOB->CRL &= ~(1U << 0);
+	// Set CNF of LED pin to alternate function output push pull
+	GPIOB->CRL |= (1U << 3);
+	GPIOB->CRL &= ~(1U << 2);
 
+	RCC->APB1ENR |= TIM3EN; // Enable clock access to timer 3
+	TIM3->PSC = 800 - 1;	// Set pre-scaler value in pre-scaler register 8 000 000 /800 = 10000
+	TIM3->ARR = 10000 - 1;	// set auto reload value 10000/10000 = 1Hz
 
-	RCC->APB1ENR |= TIM3EN;		//Enable clock access to timer 3
-	TIM3->PSC =800 - 1;  		//Set pre-scaler value in pre-scaler register 8 000 000 /800 = 10000
-	TIM3->ARR = 10000 - 1; 		//set auto reload value 10000/10000 = 1Hz
+	TIM3->CCMR2 |= OC_TOGGLE; // set bits 4 and 5 to 1 which toggles the output  Toggle - OC1REF toggles when TIMx_CNT=TIMx_CCR1. Set output compare toggle mode
 
-	TIM3->CCMR2 |=OC_TOGGLE;  //set bits 4 and 5 to 1 which toggles the output  Toggle - OC1REF toggles when TIMx_CNT=TIMx_CCR1. Set output compare toggle mode
+	// enable channel 3 in compare mode
+	TIM3->CCER |= CCER_CC3E;
 
-	//enable channel 3 in compare mode
-	TIM3->CCER |=CCER_CC3E;
-
-	TIM3->CNT = 0; 				//clear the counter value, write 0 into timer count register
-	TIM3->CR1 |= CR1_CEN;		//enable the timer
+	TIM3->CNT = 0;		  // clear the counter value, write 0 into timer count register
+	TIM3->CR1 |= CR1_CEN; // enable the timer
 }
-
-
-
-
-
 
 void Tim4_PB9_input_capture(void)
 {
-	//PB9 is connected to TIM4_CH4 by default this is physical pin 46
+	// PB9 is connected to TIM4_CH4 by default this is physical pin 46
 
-	//Enable clock access to GPIO B
-	RCC->APB2ENR |=(1U<<3);
+	// Enable clock access to GPIO B
+	RCC->APB2ENR |= (1U << 3);
 
-	//Configure the input pin
-	//SET PB9 mode to "alternate" function mode
-	//Set Mode of trace pin to input with pull up / down
-	GPIOB->CRH |=(1U<<7);
-	GPIOB->CRH &=~(1U<<6);
-		//Set CNF of trace pin to Input mode
-	//GPIOB->CRH &=~(1U<<5);
-	//GPIOB->CRH &=~(1U<<4);
-	//Enable pull up resistor
-	GPIOB->ODR |= (1U<<9);
+	// Configure the input pin
+	// SET PB9 mode to "alternate" function mode
+	// Set Mode of trace pin to input with pull up / down
+	GPIOB->CRH |= (1U << 7);
+	GPIOB->CRH &= ~(1U << 6);
+	// Set CNF of trace pin to Input mode
+	// GPIOB->CRH &=~(1U<<5);
+	// GPIOB->CRH &=~(1U<<4);
+	// Enable pull up resistor
+	GPIOB->ODR |= (1U << 9);
 
-//Set alternate function if needed here//
+	// Set alternate function if needed here//
 
-	RCC->APB1ENR |= TIM4EN;		//Enable clock access to timer 4
+	RCC->APB1ENR |= TIM4EN; // Enable clock access to timer 4
 
-	TIM4->PSC =800 - 1;  		//Set pre-scaler value in pre-scaler register 8 000 000 /800 = 10000
-	//TIM4->ARR = 10000 - 1; 		//set auto reload value 10000/10000 = 1Hz
+	TIM4->PSC = 800 - 1; // Set pre-scaler value in pre-scaler register 8 000 000 /800 = 10000
+	// TIM4->ARR = 10000 - 1; 		//set auto reload value 10000/10000 = 1Hz
 
-	TIM4->CCMR2 &=~(1U<<9);  //set bits 9 to 0 and 8 to 1 to 1 which CC4 channel is configured as input , IC4 is mapped to TI4
-	TIM4->CCMR2 |=(1U<<8);
+	TIM4->CCMR2 &= ~(1U << 9); // set bits 9 to 0 and 8 to 1 to 1 which CC4 channel is configured as input , IC4 is mapped to TI4
+	TIM4->CCMR2 |= (1U << 8);
 
-	//enable channel 4 in input mode
+	// enable channel 4 in input mode
 	TIM4->CCER |= CCER_CC4E;
 
-	//TIM4->CNT = 0; 				//clear the counter value, write 0 into timer count register
-	TIM4->CR1 |= CR1_CEN;		//enable the timer
+	// TIM4->CNT = 0; 				//clear the counter value, write 0 into timer count register
+	TIM4->CR1 |= CR1_CEN; // enable the timer
 }
