e50f6842b2c95dbe4a15a7ac01b68bdb68bdb026
Ger <gerrypdonnelly@gmail.com>
2025-12-09 21:52:09 +0000

    Got I2CMaster compiling

---
+++ Stm32F103_Blue_Pill/MAKE/InDevelopment/I2cMaster/Src/I2C.h
@@ -0,0 +1,14 @@
+#ifndef I2C_H
+#define I2C_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f103xb.h"
+
+void GpioInit(void);
+uint8_t ReadI2CData(void);
+void SetUpSlaveAddress(void);
+uint8_t ReadI2CData(void);
+void WriteI2CDataToSlave(void);
+
+#endif
--- Stm32F103_Blue_Pill/MAKE/InDevelopment/I2cMaster/Src/kernel.c
+++ Stm32F103_Blue_Pill/MAKE/InDevelopment/I2cMaster/Src/kernel.c
@@ -12,31 +12,15 @@ IF "Donnelly" is received from the slave the on board LED turns off
 #include <stdint.h>
 #include "stm32f103xb.h"
 #include "trace.h"
+#include "I2C.h"
 
-#define byte uint8_t 
-
-
-// #include "Timer.h"
-void GpioInit(void);
-uint8_t ReadData(void);
-void SetUpSlaveAddress(void);
-uint8_t ReadData(void);
-void WriteDataToSlave(void);
-
-
-void GpioInit(void)
+int main(void)
 {
-	// Set up PB6 and PB7 for I2C1
-	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;												  // Enable GPIOB clock
-	GPIOB->CRL &= ~(GPIO_CRL_MODE6 | GPIO_CRL_CNF6 | GPIO_CRL_MODE7 | GPIO_CRL_CNF7); // Clear settings
-	GPIOB->CRL |= (GPIO_CRL_MODE6_1 | GPIO_CRL_CNF6_1 | GPIO_CRL_CNF6_0 |			  // PB6 SCL
-				   GPIO_CRL_MODE7_1 | GPIO_CRL_CNF7_1 | GPIO_CRL_CNF7_0);			  // PB7 SDA
-	// Set up I2C1
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN; // Enable I2C1 clock
-	I2C1->CR1 = I2C_CR1_PE;				// Enable I2C1 peripheral
-	I2C1->CCR = 36;						// Set clock control register for 100kHz I2C clock (assuming 8MHz PCLK1)
-	I2C1->TRISE = 9;					// Set maximum rise time
-	printg("I2C1 initialized\r\n");
+	trace_init();
+	printg("Starting I2C example\r\n");
+	GpioInit();
+	SetUpSlaveAddress();
+
 	// Configure LED PB2
 	RCC->APB2ENR |= (1U << 3); // Enable clock to PB
 	// Set Mode of LED pin to output max speed 50Mhz
@@ -58,79 +42,18 @@ void GpioInit(void)
 	// SET PUll down resistor of BUTTON pin by setting ODR to 1
 	GPIOA->ODR &= ~(1U << 0);
 	printg("Push button initialized\r\n");
-}
 
-void SetUpSlaveAddress(void)
-{
-	// Set up slave address
-	I2C1->OAR1 = (0x8 << 1); // Set own address to 0x8
-	printg("I2C1 own address set to 0x8\r\n");
-}
-
-uint8_t ReadData(void)
-{
-	// Read data from slave at address 0x8
-	printg("Requesting data from slave 0x8\r\n");
-	I2C1->CR1 |= I2C_CR1_START; // Generate start condition
-	while (!(I2C1->SR1 & I2C_SR1_SB))
-		;					   // Wait for start condition generated
-	I2C1->DR = (0x8 << 1) | 1; // Send slave address with read bit
-	while (!(I2C1->SR1 & I2C_SR1_ADDR))
-		;			 // Wait for address sent
-	(void)I2C1->SR2; // Clear ADDR flag by reading SR2
-	while (!(I2C1->SR1 & I2C_SR1_RXNE))
-		;						  // Wait for data received
-	byte receivedData = I2C1->DR; // Read received data
-	printg("Received data: %d\r\n", receivedData);
-	I2C1->CR1 |= I2C_CR1_STOP; // Generate stop condition
-	printg("I2C transaction complete\r\n");
-	if (receivedData == 1)
-	{
-		GPIOB->ODR |= (1U << 2);
-	}
-	if (receivedData == 2)
-	{
-		GPIOB->ODR &= ~(1U << 2);
-	}
-	return receivedData;
-}
-
-void WriteDataToSlave(void)
-{
-	// write data to slave at address 0x8
-	printg("Sending data to slave 0x8\r\n");
-	I2C1->CR1 |= I2C_CR1_START; // Generate start condition
-	while (!(I2C1->SR1 & I2C_SR1_SB))
-		;				   // Wait for start condition generated
-	I2C1->DR = (0x8 << 1); // Send slave address with write bit
-	while (!(I2C1->SR1 & I2C_SR1_ADDR))
-		;			 // Wait for address sent
-	(void)I2C1->SR2; // Clear ADDR flag by reading SR2
-	I2C1->DR = 1;	 // Send data byte
-	while (!(I2C1->SR1 & I2C_SR1_BTF))
-		;					   // Wait for byte transfer finished
-	I2C1->CR1 |= I2C_CR1_STOP; // Generate stop condition
-	printg("Data sent to slave\r\n");
-	printg("I2C example complete\r\n");
-}
-
-int main(void)
-{
-	trace_init();
-	printg("Starting I2C example\r\n");
-	GpioInit();
-	SetUpSlaveAddress();
-
+	
 
 	while (1)
 	{
-		ReadData();
+		ReadI2CData();
 		printg("Data from slave:-\r\n");
 
 			if(GPIOA->IDR |= (1U<<3))
 		{
 			I2C1->DR = 1;
-			WriteDataToSlave();
+			WriteI2CDataToSlave();
 		}
 	}
 }
