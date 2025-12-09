aead8f223a8b6d1e3cb2367b5826dbe2772fa40c
Ger <gerrypdonnelly@gmail.com>
2025-12-09 22:38:32 +0000

    Made changes to I2C Slave

---
+++ Stm32F103_Blue_Pill/MAKE/InDevelopment/I2cSlave/Src/I2C.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f103xb.h"
+#include "trace.h"
+
+void GpioInit(void)
+{
+    // Set up PB6 and PB7 for I2C1
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;                                               // Enable GPIOB clock
+    GPIOB->CRL &= ~(GPIO_CRL_MODE6 | GPIO_CRL_CNF6 | GPIO_CRL_MODE7 | GPIO_CRL_CNF7); // Clear settings
+    GPIOB->CRL |= (GPIO_CRL_MODE6_1 | GPIO_CRL_CNF6_1 | GPIO_CRL_CNF6_0 |             // PB6 SCL
+                   GPIO_CRL_MODE7_1 | GPIO_CRL_CNF7_1 | GPIO_CRL_CNF7_0);             // PB7 SDA
+    // Set up I2C1
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN; // Enable I2C1 clock
+    I2C1->CR1 = I2C_CR1_PE;             // Enable I2C1 peripheral
+    I2C1->CCR = 36;                     // Set clock control register for 100kHz I2C clock (assuming 8MHz PCLK1)
+    I2C1->TRISE = 9;                    // Set maximum rise time
+    printg("I2C1 initialized\r\n");
+}
+
+void SetUpSlaveAddress(void)
+{
+    // Set up slave address
+    I2C1->OAR1 = (0x8 << 1); // Set own address to 0x8
+    printg("I2C1 own address set to 0x8\r\n");
+}
+
+uint8_t ReadI2CData(void)
+{
+    printg("Waiting on data from master\r\n");
+    I2C1->CR1 |= I2C_CR1_START; // Generate start condition
+    while (!(I2C1->SR1 & I2C_SR1_SB))
+        ;                      // Wait for start condition generated
+    
+    while (!(I2C1->SR1 & I2C_SR1_RXNE))
+        ;                         // Wait for data received
+    uint8_t receivedData = I2C1->DR; // Read received data
+    printg("Received data: %d\r\n", receivedData);
+    I2C1->CR1 |= I2C_CR1_STOP; // Generate stop condition
+    printg("I2C transaction complete\r\n");
+    if (receivedData == 1)
+    {
+        GPIOB->ODR |= (1U << 2);
+    }
+    if (receivedData == 2)
+    {
+        GPIOB->ODR &= ~(1U << 2);
+    }
+    return receivedData;
+}
+
+void WriteI2CDataToMaster(void)
+{
+    printg("Sending data to master\r\n");
+    I2C1->CR1 |= I2C_CR1_START; // Generate start condition
+    while (!(I2C1->SR1 & I2C_SR1_SB))
+        ;                  // Wait for start condition generated
+    I2C1->DR = (0x8 << 1); // Send slave address with write bit
+    while (!(I2C1->SR1 & I2C_SR1_ADDR))
+        ;            // Wait for address sent
+    (void)I2C1->SR2; // Clear ADDR flag by reading SR2
+    I2C1->DR = 1;    // Send data byte
+    while (!(I2C1->SR1 & I2C_SR1_BTF))
+        ;                      // Wait for byte transfer finished
+    I2C1->CR1 |= I2C_CR1_STOP; // Generate stop condition
+    printg("Data sent to Master\r\n");
+    printg("I2C example complete\r\n");
+}
--- Stm32F103_Blue_Pill/MAKE/InDevelopment/I2cSlave/Src/kernel.c
+++ Stm32F103_Blue_Pill/MAKE/InDevelopment/I2cSlave/Src/kernel.c
@@ -8,71 +8,53 @@ I2C2 - PB10 (SCL), PB11 (SDA)
  ******************************************************************************
  */
 #include <stdio.h>
+#include <stdint.h>
 #include "stm32f103xb.h"
 #include "trace.h"
-
+#include "I2C.h"
 int main(void)
 {
 	trace_init();
-	printg("Starting I2C example\r\n");
+printg("Starting I2C example\r\n");
+GpioInit();
+SetUpSlaveAddress();
+
+// Configure LED PB2
+	RCC->APB2ENR |= (1U << 3); // Enable clock to PB
+	// Set Mode of LED pin to output max speed 50Mhz
+	GPIOB->CRL |= (1U << 8);
+	GPIOB->CRL |= (1U << 9);
+	// Set CNF of LED pin to Output push pull
+	GPIOB->CRL &= ~(1U << 10);
+	GPIOB->CRL &= ~(1U << 11);
+	printg("LED initialized\r\n");
+	// Configure on board push button
+
+	RCC->APB2ENR |= (1U << 2); // Enable clock to PA
+	// Set Mode of button to input
+	GPIOA->CRL |= (1U << 3);
+	GPIOA->CRL &= ~(1U << 2);
+	// Set CNF of BUTTON pin to input
+	GPIOA->CRL &= ~(1U << 1);
+	GPIOA->CRL &= ~(1U << 0);
+	// SET PUll down resistor of BUTTON pin by setting ODR to 1
+	GPIOA->ODR &= ~(1U << 0);
+	printg("Push button initialized\r\n");
+
 	
-	//Set up PB6 and PB7 for I2C1
-	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN; //Enable GPIOB clock
-	GPIOB->CRL &= ~(GPIO_CRL_MODE6 | GPIO_CRL_CNF6 | GPIO_CRL_MODE7 | GPIO_CRL_CNF7); //Clear settings
-	GPIOB->CRL |= (GPIO_CRL_MODE6_1 | GPIO_CRL_CNF6_1 | GPIO_CRL_CNF6_0 | //PB6 SCL
-				   GPIO_CRL_MODE7_1 | GPIO_CRL_CNF7_1 | GPIO_CRL_CNF7_0); //PB7 SDA	
-	//Set up I2C1
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN; //Enable I2C1 clock
-	I2C1->CR1 = I2C_CR1_PE; //Enable I2C1 peripheral
-	I2C1->CCR = 36; //Set clock control register for 100kHz I2C clock (assuming 8MHz PCLK1)
-	I2C1->TRISE = 9; //Set maximum rise time
-	printg("I2C1 initialized\r\n");
-
-	//Set up device as slave with address 0x8
-	I2C1->OAR1 = (0x8 << 1); //Set own address to 0x8
-	printg("I2C1 set as slave with address 0x8\r\n");
-	
-	//Send "Hello from slave" when requested by master
-	const char *slaveMessage = "Hello from slave";
-	volatile int msgIndex = 0;	
-
-	//Sentry point for I2C1 event interrupts
-	NVIC_EnableIRQ(I2C1_EV_IRQn);	
-	printg("I2C1 event interrupt enabled\r\n");	
-
-	//I2C1 Event Interrupt Handler
-	void I2C1_EV_IRQHandler(void)
-	{
-		//Check for address match
-		if (I2C1->SR1 & I2C_SR1_ADDR)
-		{
-			volatile int temp = I2C1->SR2; //Read SR2 to clear
-			msgIndex = 0; //Reset message index
-		}
-		//Check for transmit buffer empty
-		if (I2C1->SR1 & I2C_SR1_TXE)
-		{
-			if (slaveMessage[msgIndex] != '\0')
-			{
-				I2C1->DR = slaveMessage[msgIndex++]; //Send next character
-			}
-			else
-			{
-				I2C1->DR = 0; //Send null character if message is done
-			}
-		}
-		//Check for stop condition
-		if (I2C1->SR1 & I2C_SR1_STOPF)
-		{
-			volatile int temp = I2C1->SR1; //Read SR1 to clear
-			I2C1->CR1 |= I2C_CR1_PE; //Re-enable peripheral
-		}
-	}
 
 
 
 	while(1)
 	{
+		ReadI2CData();
+		printg("Data from Master:-\r\n");
+
+			if(GPIOA->IDR |= (1U<<3))
+		{
+			I2C1->DR = 1;
+			WriteI2CDataToMaster();
+		}
 	}
 }
 
