dd1da0089dff232f1650060fbb8fde58d7ed2293
Gerard Donnelly <gerrypdonnelly@gmail.com>
2025-12-16 18:37:42 +0000

    made changes to lettuice saver

--- Stm32F103_Blue_Pill/MAKE/InDevelopment/Lettuice_saver/Src/kernel.c
+++ Stm32F103_Blue_Pill/MAKE/InDevelopment/Lettuice_saver/Src/kernel.c
@@ -1,10 +1,48 @@
 /**
  ******************************************************************************
-Key PA0
-LED PB2 APB2
-LED2 PB0 (external LED)
-Setting USART PA9 Tx
-Setting USART PA10 Rx
+LCD connections
+D4	PA4
+D5	PA5
+D6	PA6
+D7	PA7
+RS	PB8
+RW	PB9
+EN	PB10
+USART connections
+Tx	PA9
+Rx	PA10
+Input button connections
+Left Button B12
+Right Button B13
+Ok Button B14
+Probe connection
+Probe Button PA1
+Pump connection
+Pump Pin B15
+
+Stm32F103 Blue Pill
+				   _________
+Left		B12	--|			|--5v
+Right		B13	--|			|--G
+Ok			B14	--|			|--3v3
+Pump		B15	--|			|--R
+			A8	--|			|--B11
+Tx			A9	--|			|--B10	LCD_En
+Rx			A10	--|			|--B1
+			A11	--|			|--B0
+			A12	--|			|--A7	LCD_D7
+			A15	--|			|--A6	LCD_D6
+			B3	--|			|--A5	LCD_D5
+			B4	--|			|--A4	LCD_D4
+			B5	--|			|--A3
+			B6	--|			|--A2
+			B7	--|			|--A1	Probe ADC
+LCD_Rs		B8	--|			|--A0
+LCD_Rw		B9	--|			|--C15
+			5v	--|			|--C14
+			G	--|			|--C13
+			3v3	--|_________|--VB
+
 
 Systick timer is a 24 bit register that counts don from FFFF to 0000
 SYST_CVR Systick current value register // contains the current value
@@ -26,21 +64,138 @@ with systick being coretex-m peripheral references can be found in https://devel
  ******************************************************************************
  */
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "stm32f103xb.h"
+#include "LCD.h"
 #include "ADC.h"
 #include "trace.h"
 #include "SYSTICK.h"
 #include "Timer.h"
-#include "LCD.h"
 
 int timestamp = 0;
+uint16_t ScreenStatus = 0x00;
+uint16_t LastScreenStatus = 0x00;
+bool OkPressed;
+bool LeftPressed;
+bool RightPressed;
+uint16_t DTime = 10000; // delay time
 
-// setup : connect jumper wire from PB0 to PB9
-// timer 4 should toggle every 1000
+void WateringOn(void)
+{
+	// Set bit to turn on pump
+}
+void WateringOff(void)
+{
+	// Reset bit to turn off pump
+}
+void ReadButtons(void)
+{
+	// if(GPIOx_IDR & 0xRRRR)//OkPressed
+	//{
+	// OkPressed = 1;
+	// }
+	// else
+	//{
+	//	OkPressed = 0;
+	//}
+	// LeftPressed =
+	// RightPressed =
+}
+
 int main(void)
 {
 
-	Tim3_PB0_output_compare();
+	while (1)
+	{
+		ReadButtons();
+		if (OkPressed | LeftPressed | RightPressed)
+			ScreenStatus = 0x01;
+
+		switch (ScreenStatus)
+		{
+		case 0x00:
+			ClearScreen(); // Clear the LCD screen
+			break;
+
+		case 0x01:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Hello Allyson"); // Hello screen
+			for (int8_t i = 0; i <= DTime; i++)
+				;
+			if (OkPressed | LeftPressed | RightPressed)
+				ScreenStatus = 0x01;
+			break;
+
+		case 0x02:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Automation Stoped"); // Stop the automated system
+			break;
+
+		case 0x03:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Automation Started"); // Start the automated system
+			break;
+
+		case 0x04:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Sensor calibration"); // Calibration
+			break;
+
+		case 0x05:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Watering"); // Watering
+			while (OkPressed)
+			{
+				WateringOn();
+			}
+			WateringOff();
+			break;
+
+		case 0x06:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Settings"); // Settings menu
+			break;
+
+		case 0x07:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Watering time"); // Watering time
+			break;
+
+		case 0x08:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Moisture Start point"); // Moisture start point
+			break;
+
+		case 0x09:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("Moisture stop point"); // Moisture stop point
+			break;
+
+		case 0x0A:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("How often to test"); // How often to test
+			break;
+
+		default:
+			ClearScreen();
+			LCDGotoXY(0, 0);
+			LCDSendAString("No input"); // No input
+		}
+	}
+}
+
+/*Tim3_PB0_output_compare();
 	printg("Main loop\r\n");
 	Tim4_PB9_input_capture();
 
@@ -53,81 +208,4 @@ int main(void)
 		// read the captured counter value
 		timestamp = TIM4->CCR4;
 	}
-}
-
-void ProgramCode(void)
-{
-	uint16_t ScreenStatus = 0x00;
-	uint8_t nop;
-	uint8_t WateringOn;
-	switch (ScreenStatus)
-	{
-	case 0x00:
-		ClearScreen(); // Clear the LCD screen
-		break;
-
-	case 0x01:
-		ClearScreen();
-		LCDGotoXY(0, 0);
-		LCDSendAString("Hello Eddie"); // Hello screen
-		break;
-
-	case 0x02:
-		ClearScreen();
-		LCDGotoXY(0, 0);
-		LCDSendAString("Automation Stoped"); // Stop the automated system
-		break;
-
-	case 0x03:
-		ClearScreen();
-		LCDGotoXY(0, 0);
-		LCDSendAString("Automation Started"); // Start the automated system
-		break;
-
-	case 0x04:
-		ClearScreen();
-		LCDGotoXY(0, 0);
-		LCDSendAString("Sensor calibration"); // Calibration
-		break;
-
-	case 0x05:
-		ClearScreen();
-		LCDGotoXY(0, 0);
-		LCDSendAString("Watering"); // Watering
-		while (OkPressed)
-		{
-			WateringOn();
-		}
-		break;
-
-	case 0x06:
-		nop = 7; // Settings menu
-		break;
-
-	case 0x07:
-		nop = 8; // Watering time
-		break;
-
-	case 0x08:
-		nop = 9; // moisture start point
-		break;
-
-	case 0x09:
-		nop = 10; // moisture stop point
-		break;
-
-	case 0x0A:
-		nop = 11; // how often to test
-		break;
-
-	default:
-		nop = 11;
-	}
-}
-
-if (/*any button pressed and ScreenStatus == 0x00*/)
-{
-	LcdEnable(); // Turn on LCD
-	Set ScreenStatus == 0x01;
-}
-}
+*/
